9ead06045cc945206a46ed97b0354347296556dc
delwerhossain <delwerhossain006@gmail.com>
2023-10-03 10:51:12 +0100

    UI update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -146,14 +146,14 @@ const Header = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6">
           <div className="flex justify-between items-center border-b-2 border-gray-100 py-1 md:justify-start md:space-x-10">
             <div className="flex justify-start lg:w-0 lg:flex-1">
-              <a href="#">
+              <Link to="/">
                 <span className="sr-only">Workflow</span>
                 <img
                   className="h-20"
                   src="/image/land_logo.jpg"
                   alt="bnf logo"
                 />
-              </a>
+              </Link>
             </div>
             <div className="-mr-2 -my-2 md:hidden">
               <button
@@ -201,7 +201,7 @@ const Header = () => {
               </a>
               <Link
                to={'contact'}
-                className="text-base font-medium text-gray-500 hover:text-gray-900"
+                className="text-base font-medium bg-orange-100 p-0.5 px-2 rounded-2xl text-gray-500 hover:text-gray-900"
               >
                 Contact
               </Link>
--- src/components/NavCon/NavCon.jsx
+++ src/components/NavCon/NavCon.jsx
@@ -146,14 +146,14 @@ const NavCon = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6">
           <div className="flex justify-between items-center border-b-2 border-gray-100 py-1 md:justify-start md:space-x-10">
             <div className="flex justify-start lg:w-0 lg:flex-1">
-              <a href="#">
+              <Link to="/">
                 <span className="sr-only">Workflow</span>
                 <img
                   className="h-20"
                   src="/image/land_logo.jpg"
                   alt="bnf logo"
                 />
-              </a>
+              </Link>
             </div>
             <div className="-mr-2 -my-2 md:hidden">
               <button
@@ -182,15 +182,15 @@ const NavCon = () => {
             </div>
             <nav className="hidden md:flex space-x-10">
               <Link
-              to={'/'}
+                to={"/"}
                 className="text-base font-medium text-gray-500 hover:text-gray-900"
               >
                 Home
               </Link>
-            
+
               <Link
-               to={'contact'}
-                className="text-base font-medium text-gray-500 hover:text-gray-900"
+                to={"contact"}
+                className="text-base bg-orange-600 p-0.5 rounded-3xl  px-2 font-medium text-white hover:text-gray-900"
               >
                 Contact
               </Link>
