3288d1c1f0ec71bd79f06ecd29225a17146eb0e3
delwerhossain <delwerhossain006@gmail.com>
2023-10-03 09:48:33 +0100

    conditional navbar added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
+import NavCon from "../components/NavCon/NavCon";
 
 const Main = () => {
+  const { pathname } = useLocation();
+  const nav = () => {
+    if (pathname === "/contact") {
+      return <NavCon />;
+    } else {
+      return <Header />;
+    }
+  };
+
   return (
     <>
-      <Header />
+      {nav()}
       <div className="max-w-7xl mx-auto">
         <Outlet />
       </div>
