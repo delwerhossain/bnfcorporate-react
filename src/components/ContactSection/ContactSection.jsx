f362a8912bce9562bdba42a5329e34f6934ef3a8
delwerhossain <delwerhossain006@gmail.com>
2023-10-04 16:02:14 +0100

    ContactSection update

--- src/components/ContactSection/ContactSection.jsx
+++ src/components/ContactSection/ContactSection.jsx
@@ -26,10 +26,7 @@ const ContactSection = () => {
           <h3 className="text-2xl text-slate-600 my-3 font-bold">
             Office Address
           </h3>
-          <p>
-            <span className="font-bold">Chowdhury Info Tech</span>, CDA,
-            Colonelhut, Chittagong, Bangladesh.
-          </p>
+          <p>CDA, 17 Mill Lane, Leicester, LE7 1NS.</p>
         </div>
         <div className="p-6 col-span-1 border border-orange-600 rounded-xl hover:bg-orange-100 ">
           <div className=" rounded-full border  border-dashed border-orange-600 p-4 mx-auto w-16">
@@ -41,7 +38,7 @@ const ContactSection = () => {
             className="text-orange-600 text-sm lg:text-lg"
             href="mailto:contact@thebandf.com"
           >
-            contact @thebandf.com
+            info@bnfcorporate.com
           </a>
         </div>
         <div className="p-6 col-span-1 border border-orange-600 rounded-xl hover:bg-orange-100 ">
@@ -51,7 +48,7 @@ const ContactSection = () => {
           </div>
           <h3 className="text-2xl text-slate-600 my-3 font-bold">Call Us</h3>
           <a className="text-orange-600" href="tel:+880 1819 331933">
-            +880 1819 331933
+            03301 130 458
           </a>
         </div>
       </div>
@@ -130,10 +127,19 @@ const ContactSection = () => {
           </div>
         </section>
 
+        {/* <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2417.4618465834565!2d-1.0910530999999999!3d52.7058087!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4879df531e027733%3A0xee9d9e9f02ccf64f!2sE%20Syston%20Mill%2C%2017%20Mill%20Ln%2C%20Syston%2C%20Leicester%20LE7%201NS%2C%20UK!5e0!3m2!1sen!2sbd!4v1696431277999!5m2!1sen!2sbd"
+          width="600"
+          height="450"
+          style="border:0;"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe> */}
         <div className="md:mb-12 lg:mb-0 ">
           <div className="relative h-[780px] rounded-lg shadow-lg">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1304.4342670057342!2d91.77557077731313!3d22.372603946247544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1606837466884!5m2!1sen!2sbd"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2417.4618465834565!2d-1.0910530999999999!3d52.7058087!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4879df531e027733%3A0xee9d9e9f02ccf64f!2sE%20Syston%20Mill%2C%2017%20Mill%20Ln%2C%20Syston%2C%20Leicester%20LE7%201NS%2C%20UK!5e0!3m2!1sen!2sbd!4v1696431277999!5m2!1sen!2sbd"
               className="absolute left-0 top-0 h-full w-full rounded-lg border border-orange-600"
               frameBorder="0"
               allowFullScreen
